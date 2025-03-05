652bba184ef8ac1a78d59167f5ed8a7fcd961a97
Fening Okai <feningokai@outlook.com>
2025-03-05 08:17:37 -0600

    Remove test files from repository

--- jest.setup.js
+++
@@ -1,8 +0,0 @@
-// Learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-import { fetch, Request, Response } from 'cross-fetch';
-
-// Make fetch available globally
-global.fetch = fetch;
-global.Request = Request;
-global.Response = Response;
